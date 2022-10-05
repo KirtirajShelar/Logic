c1353768510bfbd63ce085caef5d0da2d72d74d2
Your Name <KirtirajShelar@kirtirajshelar228.com>
2022-10-05 16:58:01 +0530

    Swap Numbers Using Arithmetic Operator

---
+++ Logic/src/SwapNumbersUsingArithmeticOperator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SwapNumbersUsingArithmeticOperator {
+public static void main(String[] args) {
+
+	int a,b,t;
+	 System.out.println("Enter the number a: ");
+	 Scanner sc = new Scanner(System.in);
+	 a = sc.nextInt();
+	 System.out.println("Enter the number b: ");
+	 b = sc.nextInt();
+	 
+	 a=a+b;
+	 b=a-b;
+	 a=a-b;
+	 System.out.println("Values after swapping a="+a+" and b="+b);
+}
+}
